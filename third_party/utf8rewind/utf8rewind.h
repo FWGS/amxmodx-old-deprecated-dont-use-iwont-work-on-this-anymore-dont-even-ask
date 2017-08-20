67f08fef158a2223f1fcd4460813551c6aca74b5
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-08-20 20:05:59 +0600

    Fix build on Ubuntu 17.04, gcc 6.3.0

--- third_party/utf8rewind/utf8rewind.h
+++ third_party/utf8rewind/utf8rewind.h
@@ -68,6 +68,10 @@
 #include <stdint.h>
 #include <string.h>
 #include <wchar.h>
+#ifdef __linux__
+	#include <strings.h> // strncasecmp
+	#include <sys/types.h> // off_t
+#endif
 
 /*!
 	\addtogroup version
